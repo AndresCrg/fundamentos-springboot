cd8928ba506b851b32790ad43219a86f3ff4acea
AndresCrg <36751170+AndresCrg@users.noreply.github.com>
2022-10-15 20:54:08 -0500

    primerisimo commit

---
+++ src/test/java/com/platzi/ejercicio1/Ejercicio1PlatziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.ejercicio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio1PlatziApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
